1068ee41d07f46f0b126b672313c25a14b586293
Jannatut <59221736+Jannatut@users.noreply.github.com>
2021-09-13 17:42:41 +0600

    created a file

---
+++ day_6/accessAlignment.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyDay6App());
+}
+class MyDay6App extends StatelessWidget {
+  const MyDay6App({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Day6 App"),
+          ),
+          body: Container(
+            color: Colors.white,
+            
+              child: Column(
+                
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: 
+              [Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(onPressed: (){
+                  print(Text("textdemo1"));
+                }, child: Text("click1")),
+              ),Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                  width: 60.0,
+                  height: 90.0,
+                  child: ElevatedButton(onPressed: (){
+                    print(Text("demo text2"));
+                  }, child: Text("click2")),
+                ),
+              )],),
+            
+            ),
+          ),
+          
+      
+    );
+  }
+}
